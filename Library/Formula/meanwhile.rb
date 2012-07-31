2c3896ebad60451782dc5b734ca1d142d39e9a7d
Chris Parker <mrcsparker@gmail.com>
2012-07-31 00:44:04 -0500

    Initial commit

---
+++ Library/Formula/meanwhile.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Meanwhile < Formula
+  url 'https://github.com/downloads/mrcsparker/meanwhile/meanwhile-1.1.1.tar.gz'
+  sha1 'dee577b129f123448c2dfbc93640e827830c3090'
+  homepage 'http://meanwhile.sourceforge.net'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'glib'
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-debug",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+# My homebrew repository
+
+## Includes
+
+* meanwhile-1.1.1
